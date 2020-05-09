c58fdc16155e4286e2c83d28fb2701d6be2b06fb
matthew-devonport <roarsintheeast@gmail.com>
2020-05-09 12:30:49 +1200

    testing pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,30 @@ import {graphql, StaticQuery} from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+<StaticQuery query={graphql`
+{
+  allWordpressPage {
+    edges {
+      node {
+        title
+        content
+      }
+    }
+  }
+}
+`} render={props => (
+  <div>
+    {props.allWordpressPage.edges.map(page => (
+      <div key={page.node.id}>
+        <h1>
+          {page.node.title}
+        </h1>
+        <div dangerouslySetInnerHTML={{__html: page.node.content}}></div>
+      </div>
+    ))}
+  </div>
+)} />
+    <Link to="/page-2">Go to page 2</Link>
   </Layout>
 )
 
