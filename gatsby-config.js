71570ce1dff567c0911f0cf9939ea065ba0f4f63
matthew-devonport <roarsintheeast@gmail.com>
2020-05-10 15:24:08 +1200

    added menus to includedRoutes

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,6 +115,7 @@ module.exports = {
           "**/tags",
           "**/taxonomies",
           "**/users",
+          "**/menus"
         ],
         // Blacklisted routes using glob patterns
         excludedRoutes: ["**/posts/1456"],
