8463db50b9f9e97d7445fc05b48d36e3d9ac8574
matthew-devonport <roarsintheeast@gmail.com>
2020-05-10 18:17:48 +1200

    linking between pages on mainmenu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,10 +1,29 @@
 import React from 'react'
-// import { graphql, StaticQuery } from 'gatsby'
+import { graphql, StaticQuery, Link } from 'gatsby'
 
 const MainMenu = () => (
+<StaticQuery query={graphql`
+{
+  allWordpressWpApiMenusMenusItems {
+    edges {
+      node {
+        items {
+          title
+          object_slug
+        }
+      }
+    }
+  }
+}
+`} render={props => (
     <div>
-        Main Menu
+        {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => (
+            <Link to={item.object_slug} key={item.title}>
+                {item.title}
+            </Link>
+        ))}
     </div>
+)}/>
 )
 
 export default MainMenu;
