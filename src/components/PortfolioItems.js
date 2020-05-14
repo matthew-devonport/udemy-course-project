8a72dc814cb8dc353b806896bf168a4752a9d87e
matthew-devonport <roarsintheeast@gmail.com>
2020-05-14 23:46:28 +1200

    added styles to portfolio pages

--- src/components/PortfolioItems.js
+++ src/components/PortfolioItems.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {graphql, StaticQuery, Link} from 'gatsby'
+import styled from 'styled-components'
 
 
 const PortfolioItems = () => {
--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Layout from '../components/layout'
+import styled from 'styled-components'
+
+const FeaturedImage = styled.img`
+max-width: 300px;
+margin: 16px 0;
+`
 
 export default ({pageContext}) => (
-    <div>
+    <Layout>
 <h1>
     {pageContext.title}
 </h1>
-    </div>
+<FeaturedImage src={pageContext.featured_media.source_url} />
+<div dangerouslySetInnerHTML={{__html: pageContext.content}} />
+    </Layout>
 )
