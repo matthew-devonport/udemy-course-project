9bbffa60a89042230caf827b1b26ea2f4648474d
matthew-devonport <roarsintheeast@gmail.com>
2020-05-15 22:13:26 +1200

    added better styles to page numbers

--- src/templates/blogPostList.js
+++ src/templates/blogPostList.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import Layout from '../components/layout'
 import {Link} from 'gatsby'
+import styled from 'styled-components'
 
+const Pagination = styled.div`
+ display: flex;
+ justify-content: flex-end;
+`
+
+const PageNumberWrapper = styled.div`
+ border: 1px solid #eee;
+ background: ${props => props.isCurrentPage ? '#eee' : 'white'}
+`
+
+const PageNumber = styled(Link)`
+ display: block;
+ padding: 8px 16px;
+`
 
 export default ({pageContext}) => (
     <Layout>
@@ -11,12 +26,14 @@ export default ({pageContext}) => (
                 <p dangerouslySetInnerHTML={{__html: post.node.excerpt}} />
             </div>
         ))}
+        <Pagination>
         {Array.from({length: pageContext.numberOfPages}).map((page, index) => (
-            <div key={index}>
-                <Link to={index === 0 ? `/blog` : `/blog/${index + 1}`}>
+            <PageNumberWrapper  key={index} isCurrentPage={index + 1 === pageContext.currentPage}>
+                <PageNumber to={index === 0 ? `/blog` : `/blog/${index + 1}`}>
                   {index + 1}
-                </Link>
-            </div>
+                </PageNumber>
+            </PageNumberWrapper>
         ))}
+        </Pagination>
     </Layout>
 )
