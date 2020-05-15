c823556c3c2c4d42d912e78ca9b8d8cbe39c7620
matthew-devonport <roarsintheeast@gmail.com>
2020-05-15 23:17:21 +1200

    readmore links and displays content correctly

--- src/templates/blogPostList.js
+++ src/templates/blogPostList.js
@@ -28,7 +28,7 @@ export default ({pageContext}) => (
                 </small>
                 <p dangerouslySetInnerHTML={{__html: post.node.excerpt}} />
                 <div>
-                    <Link to={`/${post.node.slug}`}>
+                    <Link to={`/post/${post.node.slug}`}>
                     Read More
                     </Link>
                 </div>
