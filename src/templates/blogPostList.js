6c9ad85eacd96a8cd9527c3023a86e9c7ca53671
matthew-devonport <roarsintheeast@gmail.com>
2020-05-15 17:20:54 +1200

    blog pages rendering correctly

--- src/templates/blogPostList.js
+++ src/templates/blogPostList.js
@@ -4,6 +4,11 @@ import Layout from '../components/layout'
 
 export default ({pageContext}) => (
     <Layout>
-        Blog
+        {pageContext.posts.map(post => (
+            <div>
+                <h3 dangerouslySetInnerHTML={{__html: post.node.title}} />
+                <p dangerouslySetInnerHTML={{__html: post.node.excerpt}} />
+            </div>
+        ))}
     </Layout>
 )
