1714fd6dbefbf7b28e299e45e2bd01dcb6034eea
matthew-devonport <roarsintheeast@gmail.com>
2020-05-15 15:40:13 +1200

    added url to port pages/fixed some bugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,6 +48,9 @@ exports.createPages = async ({ graphql, actions }) => {
 					featured_media{
 					  source_url
 					}
+					acf{
+					  portfolio_url
+					}
 				  }
 				}
 			  }
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import PortfolioItems from '../components/PortfolioItems'
+
 
 export default ({pageContext}) => (
     <Layout>
